a994f9dd89a9493a3e6463766a2b1150bd1b8fb1
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-09-24 23:01:51 +0300

    Add files via upload

---
+++ C++/Beginner - Reduce but Grow CodeWars/Beginner - Reduce but Grow CodeWars.cpp
@@ -0,0 +1,26 @@
+﻿/*
+* Given a non-empty array of integers, return the result of multiplying the values together in order. 
+*
+* Example: [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+*/
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int grow(vector<int> nums)
+{
+    int a = 1;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int value = nums[i];
+        a = a * value;
+    }
+    return a;
+}
+
+int main()
+{
+    vector<int>nums = { 1, 2, 3 };
+    cout << grow(nums) << endl;
+}

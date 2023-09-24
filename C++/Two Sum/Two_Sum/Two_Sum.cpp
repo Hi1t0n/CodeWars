b92d9ea3e068bd9a574687c012d8ea5ee30b48e3
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-09-24 23:17:30 +0300

    Add files via upload

---
+++ C++/Two Sum/Two_Sum/Two_Sum.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+vector <int> twosum(const std::vector<int>& numbers, int target)
+{
+    for (int i = 0; i < numbers.size()-1; i++)
+    {
+        for (int j = i + 1; j < numbers.size(); j++) {
+            if (numbers[i] + numbers[j] == target)
+            {
+                return {i ,j};
+            }
+        }
+    }
+    return {};
+}
+
+int main()
+{
+    vector<int>nums = { 1, 2, 3 };
+    int target = 14690;
+    //twosum({ 1234, 5678, 9012 }, 14690);
+    for (int &x : twosum({ 1234, 5678, 9012 }, target))
+    {
+        cout << x << endl;
+    }
+    return 0;
+}
---
+++ C++/Vowel Count/Vowel_Count/Vowel_Count.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int getCount(const string& inputStr) {
+	int num_vowels = 0;
+	char abc[] = { 'a', 'e', 'i','o','u'};
+	for (int i = 0; i < inputStr.length(); i++)
+	{
+		for (int j = 0; j < 5; j++) {
+			if (inputStr[i] == abc[j])
+			{
+				num_vowels++;
+			}
+		}
+	}
+	return num_vowels;
+}
+
+int main() {
+	setlocale(LC_ALL, "russian");
+
+	string text = "";
+	getline(cin, text);
+	
+	cout<< getCount(text);
+	return 0;
+}

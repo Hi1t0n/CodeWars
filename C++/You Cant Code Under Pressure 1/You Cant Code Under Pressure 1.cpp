b02fe82753f1e3fd1e98c9e071464efe5607f711
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-09-24 23:18:38 +0300

    Add files via upload

---
+++ C++/You Cant Code Under Pressure 1/You Cant Code Under Pressure 1.cpp
@@ -0,0 +1,10 @@
+// Code as fast as you can! You need to double the integer and return it.
+#include <iostream>
+int32_t double_integer(int32_t n)
+{
+    return n * 2;
+}
+int main()
+{
+    std::cout << double_integer(40);
+}

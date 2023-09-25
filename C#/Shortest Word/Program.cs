092d6e4b8774cdcce1d14eefa5d87b75ff9a15bd
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-09-26 00:11:27 +0300

    Add files via upload

---
+++ C#/Shortest Word/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+ */
+
+namespace Shortest_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata.FindShort("String will never be empty and you do not need to account for different data types")
+        }
+    }
+}
+
+public class Kata
+{
+    public static int FindShort(string s)
+    {
+        string[] Words = s.Split(' ');
+        Array.Sort(Words, (x,y) => x.Length.CompareTo(y.Length));
+        return Words[0].Length;
+    }
+}

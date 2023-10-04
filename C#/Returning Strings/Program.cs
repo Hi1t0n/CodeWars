d262127f06e640ed9fcfdc52ecc789c1aff9157d
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-04 13:53:31 +0300

    Add files via upload

---
+++ C#/Returning Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Returning_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Greet("Hilton"));
+        }
+    }
+}
+
+public static class Kata
+{
+    public static string Greet(string name) => $"Hello, {name} how are you doing today?";
+}

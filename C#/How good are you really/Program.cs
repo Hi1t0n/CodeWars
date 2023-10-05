bc1956975ed04d7ae9bfaa2b5122a86a79035589
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-05 23:28:24 +0300

    Add files via upload

---
+++ C#/How good are you really/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace How_good_are_you_really
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.BetterThanAverage(new int[] { 100, 40, 34, 57, 29, 72, 57, 88 }, 75));
+        }
+    }
+}
+
+
+public class Kata
+{
+    public static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
+    {
+        return YourPoints > ClassPoints.Average();
+    }
+}

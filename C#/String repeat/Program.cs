e64edf918e4549b8df68a705d470e07ffff770b2
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-06 23:07:22 +0300

    Add files via upload

---
+++ C#/String repeat/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace String_repeat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.Program.RepeatStr(3,"aa"));
+        }
+    }
+}
+
+namespace Solution
+{
+    public static class Program
+    {
+        public static string RepeatStr(int n, string s)
+        {
+            return string.Join("",Enumerable.Repeat(s, n));
+        }
+    }
+}

d6f650473638d5ec4729f3c0c8e92c4cbbd759c5
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-10 21:57:17 +0300

    Add files via upload

---
+++ C#/Color Ghost/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Color_Ghost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ghost ghost = new Ghost();
+            Console.WriteLine(ghost.GetColor());
+        }
+    }
+}
+
+
+public class Ghost
+{
+
+    public Ghost() => color = availableColor[new Random().Next(0, availableColor.Length)];
+    public string GetColor() { return color; }
+
+    private string[] availableColor = { "yellow", "white", "purple", "red" };
+    public string color;
+}

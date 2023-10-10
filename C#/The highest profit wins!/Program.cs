b9d54fb6602ccc43ae3c4ed65ee6007f43acd03a
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-10 22:12:57 +0300

    Add files via upload

---
+++ C#/The highest profit wins!/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+namespace The_highest_profit_wins_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = MinMax.minMax(new int[] { 1, 2, 5, -1, 12, 20 });
+            foreach (int i in arr) { Console.WriteLine(i); }
+        }
+    }
+}
+
+public class MinMax
+{
+    public static int[] minMax(int[] lst)
+    {
+        return lst.Length is not 0 ? new int[] { lst.Min(), lst.Max()} : lst; 
+    }
+}

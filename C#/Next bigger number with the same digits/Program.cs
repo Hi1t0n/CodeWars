fb12ab80b5b95b2c726f7ecbb513a8161ffb4300
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-17 00:04:55 +0300

    Add files via upload

---
+++ C#/Next bigger number with the same digits/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Next_bigger_number_with_the_same_digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.NextBiggerNumber(513));
+            Console.WriteLine(Kata.NextBiggerNumber(49424214));
+        }
+    }
+}
+
+public class Kata
+{
+    public static long NextBiggerNumber(long n)
+    {
+        int[] digits = n.ToString().Select(x=> int.Parse(x.ToString())).ToArray();
+        int pivotIndex = -1;
+        for (int i = digits.Length-2; i >= 0; i--) 
+        {
+            if (digits[i] < digits[i+1])
+            {
+                pivotIndex = i;
+                break;
+            }
+        }
+        if (pivotIndex is -1) { return  pivotIndex; }
+
+        int swapIndex = -1;
+        for(int i = digits.Length-1; i >= 0; i--) 
+        {
+            if (digits[i] > digits[pivotIndex])
+            {
+                swapIndex = i;
+                break;
+            }
+        }
+
+        int temp = digits[pivotIndex];
+        digits[pivotIndex] = digits[swapIndex];
+        digits[swapIndex] = temp;
+        Array.Sort(digits, pivotIndex + 1, digits.Length - pivotIndex - 1);
+        long result = long.Parse(string.Join("",digits));
+        return result > n ? result : -1;
+    }
+}

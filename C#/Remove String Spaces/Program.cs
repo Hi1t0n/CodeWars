244b519b89eb94d49b3f25344a0979aa683b833e
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-17 00:35:08 +0300

    Add files via upload

---
+++ C#/Remove String Spaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using Solution;
+
+namespace Remove_String_Spaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SpacesRemover.NoSpace("Ba nan fn f df ds fnndf nsdn n nfdnndsnf nf nn  nn n n "));
+        }
+    }
+}
+
+namespace Solution
+{
+    public static class SpacesRemover
+    {
+        public static string NoSpace(string input) =>  input.Replace(" ", "");
+    }
+}

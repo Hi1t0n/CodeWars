54897dda0f8c21ec0c44ee157d13327e3f94df5b
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-17 00:30:23 +0300

    Add files via upload

---
+++ C#/Isograms/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+namespace Isograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.IsIsogram("ban"));
+            Console.WriteLine(Kata.IsIsogram("banan"));
+        }
+    }
+}
+
+
+public class Kata
+{
+    public static bool IsIsogram(string str)
+    {
+        str = str.ToLower();
+        for (int i = 0; i < str.Length; i++)
+        {
+            int val = str.Count(x => x == str[i]);
+            if(val > 1) { return false; }
+        }
+        return true;
+    }
+}

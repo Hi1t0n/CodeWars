bc94a00db50c63cfad00d70e5b83a82e2754df78
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-20 01:04:56 +0300

    Add files via upload

---
+++ C#/Find the missing letter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Find_the_missing_letter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.FindMissingLetter(new[] { 'a', 'b', 'c', 'd', 'f' }));
+        }
+    }
+}
+
+public class Kata
+{
+    public static char FindMissingLetter(char[] array)
+    {
+        char prevChar = array[0];
+        char existChar;
+        for (int i = 1; i < array.Length; i++)
+        {
+            existChar = array[i];
+            int difference = (int)existChar - (int)prevChar;
+            if (difference > 1) { break; }
+            prevChar = existChar;
+        }
+        return Convert.ToChar((int)prevChar + 1);
+    }
+}

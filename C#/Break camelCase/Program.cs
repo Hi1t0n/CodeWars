e8ebb45521d8e962a32ac8c46fda3189e7dd6c9f
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-20 00:31:27 +0300

    Add files via upload

---
+++ C#/Break camelCase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Break_camelCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.BreakCamelCase("camelCase"));
+        }
+    }
+}
+
+public class Kata
+{
+    public static string BreakCamelCase(string str)
+    {
+        string res = "";
+        string[] words = Regex.Split(str, "(?=\\p{Lu})");
+        foreach (string word in words)
+        {
+            res += word + " ";
+        }
+        return res[..^1];
+
+    }
+}

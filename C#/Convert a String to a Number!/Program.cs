f3dc7bdaf93c6c068235270cec6208d5fe7ddd44
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-20 00:39:34 +0300

    Add files via upload

---
+++ C#/Convert a String to a Number!/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Convert_a_String_to_a_Number_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.StringToNumber("1405"));
+            Console.WriteLine(Kata.StringToNumber("77"));
+            Console.WriteLine(Kata.StringToNumber("-7"));
+        }
+    }
+}
+
+
+public class Kata
+{
+    public static int StringToNumber(String str) => Int32.Parse(str);
+}

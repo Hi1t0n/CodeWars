76acb7a9494921946b70a1d1da147867d6f39c6d
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-22 23:42:52 +0300

    Add files via upload

---
+++ C#/Count by X/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Count_by_X
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] res = Kata.CountBy(2, 5);
+            foreach (int i in res)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+public static class Kata
+{
+    public static int[] CountBy(int x, int n)
+    {
+        int[] res = new int[n];
+        for (int i = x, j = 0 ; j < n; i += x,j++)
+        {
+            res[j] = i;
+        }
+
+        return res;
+    }
+}

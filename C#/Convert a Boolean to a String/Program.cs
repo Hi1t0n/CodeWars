253fdfa2ff8e0a3f6037f8e8f0179921209ef9f3
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-23 20:59:44 +0300

    Add files via upload

---
+++ C#/Convert a Boolean to a String/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Convert_a_Boolean_to_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(kata.BooleanToString(true));
+        }
+    }
+}
+
+public class kata
+{
+    public static string BooleanToString(bool b) => b.ToString();
+}

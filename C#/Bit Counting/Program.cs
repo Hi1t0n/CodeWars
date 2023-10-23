92516b56b481488f40fa919559128564839a9e06
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-23 20:35:20 +0300

    Add files via upload

---
+++ C#/Bit Counting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System;
+
+namespace Bit_Counting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.CountBits(1234));   
+        }
+    }
+}
+
+
+
+public class Kata
+{
+    public static int CountBits(int n)
+    {
+        string bitVal = Convert.ToString(n,2);
+        int count = 0;
+        foreach (char c in bitVal) { if(c == '1') { count++; } }
+        return count;
+    }
+}

80913d13149f63d54cd1681bc94f3ad630a8ce37
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-23 21:04:59 +0300

    Add files via upload

---
+++ C#/Century From Year/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Century_From_Year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.СenturyFromYear(2000));
+            Console.WriteLine(Kata.СenturyFromYear(1705));
+        }
+    }
+}
+
+public static class Kata
+{
+    public static int СenturyFromYear(int year) => year % 100 == 0 ? year / 100 : year / 100 + 1;
+}

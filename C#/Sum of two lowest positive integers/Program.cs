9ff8c05cda972e20b7f8656b6c1b41393a3544a2
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-23 21:26:17 +0300

    Add files via upload

---
+++ C#/Sum of two lowest positive integers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Sum_of_two_lowest_positive_integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata.sumTwoSmallestNumbers(new int[] { 2,4,1,3,2,1,78,567,46,2,62,653});
+        }
+    }
+}
+
+public static class Kata
+{
+    public static int sumTwoSmallestNumbers(int[] numbers)
+    {
+        numbers = numbers.OrderBy(x => x).ToArray();
+        return numbers[0]+numbers[1];
+    }
+}

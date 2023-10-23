4076688955b03b385db580d4d2234f158086689a
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-23 21:17:37 +0300

    Add files via upload

---
+++ C#/Moving Zeros To The End/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Moving_Zeros_To_The_End
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Kata.MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 });
+            foreach (int i in arr) { Console.WriteLine(i); }
+        }
+    }
+}
+
+public class Kata
+{
+    public static int[] MoveZeroes(int[] arr)
+    {
+        int[] values = arr.Where(x => x != 0).ToArray();
+        int[] zeroArray = arr.Where(x => x == 0).ToArray();
+
+        return values.Concat(zeroArray).ToArray();
+    }
+}

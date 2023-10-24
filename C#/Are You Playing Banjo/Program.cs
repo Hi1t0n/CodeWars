5588ad5797e00e76c0d6e00c8bc5886a3066c93a
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-24 23:27:14 +0300

    Add files via upload

---
+++ C#/Are You Playing Banjo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Are_You_Playing_Banjo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+public class Kata
+{
+    public static string AreYouPlayingBanjo(string name) => name[0].ToString().ToLower() is "r" ? $"{name} plays banjo" : $"{name} does not play banjo";
+}

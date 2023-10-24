048ea9d0b26243fc10c4eca20d7aab12a2c61ceb
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-24 23:37:46 +0300

    Add files via upload

---
+++ C#/Find The Parity Outlier/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Find_The_Parity_Outlier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Find(new int[] { 2, 6, 8, -10, 3 });
+        }
+    }
+}
+
+
+
+public class Kata
+{
+    public static int Find(int[] integers)
+    {
+        int[] res = integers.Where(x => x % 2 == 0).ToArray();
+        return res.Length == 1 ? res[0] : Convert.ToInt32(integers.First(x => x % 2 != 0));
+    }
+}

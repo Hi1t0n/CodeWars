0f60a2b2dbb29aa6e5d2a2776fffea8289143983
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-24 23:40:37 +0300

    Add files via upload

---
+++ C#/Opposite number/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Opposite_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Opposite(1));
+            Console.WriteLine(Kata.Opposite(2311));
+            Console.WriteLine(Kata.Opposite(-1));
+        }
+    }
+}
+
+public class Kata
+{
+    public static int Opposite(int number) => number * -1;
+}

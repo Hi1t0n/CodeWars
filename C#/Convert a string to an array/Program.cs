031985c349adb487c9d1b80c2910bdb4ba0a4d28
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-25 01:14:15 +0300

    Add files via upload

---
+++ C#/Convert a string to an array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Convert_a_string_to_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Solution.Solution.StringToArray("I love arrays they are my favorite");
+            foreach (string word in words) { Console.WriteLine(word); }
+        }
+    }
+}
+
+namespace Solution
+{
+    public class Solution
+    {
+        public static string[] StringToArray(string str) => str.Split(' ');
+    }
+}

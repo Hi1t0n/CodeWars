a3faae2959425cdb62307d8cef94e0819309d5bb
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-25 18:26:59 +0300

    Add files via upload

---
+++ C#/Array plus array/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Array_plus_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.ArrayPlusArray(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }));
+        }
+    }
+}
+
+public static class Kata
+{
+    public static int ArrayPlusArray(int[] arr1, int[] arr2) => arr1.Sum() + arr2.Sum();
+}

f0b7ac308c323a26da5460f99ad56d34d273b900
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-27 01:38:35 +0300

    Add files via upload

---
+++ C#/Get the mean of an array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Get_the_mean_of_an_array
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.GetAverage(new int[]{2,2,2,2}));
+            Console.WriteLine(Kata.GetAverage(new int[]{5,10}));
+        }
+    }
+}
+
+public class Kata
+{
+    public static int GetAverage(int[] marks) => (int)Math.Floor(marks.Sum() / (double)marks.Length);
+}

31e27651d5b20eb6972682d282896bfb80603e37
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-27 00:54:40 +0300

    Add files via upload

---
+++ C#/The Feast of Many Beasts/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace The_Feast_of_Many_Beasts
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Feast("great blue heron", "garlic naan"));
+        }
+    }
+}
+
+public class Kata
+{
+    public static bool Feast(string beast, string dish) =>
+        beast[0] == dish[0] && beast[beast.Length - 1] == dish[dish.Length - 1];
+}

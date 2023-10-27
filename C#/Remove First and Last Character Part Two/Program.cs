daec83d5bd34c7d2da0f03d30d628d029dd318ae
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-27 23:46:27 +0300

    Add files via upload

---
+++ C#/Remove First and Last Character Part Two/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Remove_First_and_Last_Character_Part_Two
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Array("1,2,3"));
+        }
+    }
+}
+
+public static class Kata
+{
+    public static string Array(string s)
+    {
+        if (s.Length <= 2) return null;
+        string[] arr = s.Split(',');
+        if (arr.Length <= 2) return null;
+        return string.Join(" ", arr, 1, arr.Length - 2);
+    }
+}

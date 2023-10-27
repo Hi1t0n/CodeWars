423dfbb43fcd4e708fd26724f2304fb5f7810143
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-10-27 23:53:52 +0300

    Add files via upload

---
+++ C#/Function 1 - hello world/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace Function_1___hello_world
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.greet());
+        }
+    }
+}
+
+public static class Kata
+{
+    public static string greet() => "hello world!";
+}

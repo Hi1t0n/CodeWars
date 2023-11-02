fc1d1ac953a57842a51457513b77d3f819b8364d
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-02 21:47:53 +0300

    Add files via upload

---
+++ C#/Thinkful - Logic Drills Traffic light/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Thinkful___Logic_Drills_Traffic_light
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.UpdateLight("green"));
+            Console.WriteLine(Kata.UpdateLight("yellow"));
+            Console.WriteLine(Kata.UpdateLight("red"));
+        }
+    }
+}
+
+public class Kata
+{
+    public static string UpdateLight(string current)
+    {
+        switch (current)
+        {
+            case "green":
+                return "yellow";
+            case "yellow":
+                return "red";
+            case "red":
+                return "green";
+            default:
+                return null;
+        }
+    }
+}

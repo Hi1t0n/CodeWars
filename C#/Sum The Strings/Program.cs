2d416975200f689c06f8c223d2248c1ea863a683
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-02 21:53:57 +0300

    Add files via upload

---
+++ C#/Sum The Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace Sum_The_Strings
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.Program.StringsSum("1","2"));
+            Console.WriteLine(Solution.Program.StringsSum("",""));
+        }
+    }
+}
+
+namespace Solution
+{
+    public static class Program
+    {
+        public static string StringsSum(string s1, string s2)
+        {
+            if (s1.Length == 0) {s1 = "0";}
+            if (s2.Length == 0) {s2 = "0";}
+            return (int.Parse(s1) + int.Parse(s2)).ToString();
+        }
+    }
+}

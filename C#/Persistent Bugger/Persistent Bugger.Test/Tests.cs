9dcf3036137e991ed60800273cc8f8e6843a2579
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-02 22:34:21 +0300

    Add files via upload

---
+++ C#/Persistent Bugger/Persistent Bugger.Test/Tests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Persistent_Bugger.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Persistence_39()
+        {
+            int value = 39;
+            long result = Persist.Persistence(value);
+            long exResult = 3;
+            Assert.AreEqual(result, exResult);
+        }
+        [Test]
+        public void Persistence_4()
+        {
+            int value = 4;
+            long result = Persist.Persistence(value);
+            long exResult = 0;
+            Assert.AreEqual(result, exResult);
+        }
+        [Test]
+        public void Persistence_25()
+        {
+            int value = 25;
+            long result = Persist.Persistence(value);
+            long exResult = 2;
+            Assert.AreEqual(result, exResult);
+        }
+        [Test]
+        public void Persistence_999()
+        {
+            int value = 999;
+            long result = Persist.Persistence(value);
+            long exResult = 4;
+            Assert.AreEqual(result, exResult);
+        }
+
+        [Test]
+        public void Persistence_1234()
+        {
+            int value = 1234;
+            long result = Persist.Persistence(value);
+            long exResult = 2;
+            Assert.AreEqual(result,exResult);
+        }
+        
+        [Test]
+        public void Persistence_5678()
+        {
+            int value = 5678;
+            long result = Persist.Persistence(value);
+            long exResult = 2;
+            Assert.AreEqual(result,exResult);
+        }
+        
+        [Test]
+        public void Persistence_1111()
+        {
+            int value = 1111;
+            long result = Persist.Persistence(value);
+            long exResult = 1;
+            Assert.AreEqual(result,exResult);
+        }
+        
+        [Test]
+        public void Persistence_987654()
+        {
+            int value = 987654;
+            long result = Persist.Persistence(value);
+            long exResult = 2;
+            Assert.AreEqual(result,exResult);
+        }
+    }
+}
---
+++ C#/Persistent Bugger/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Persistent_Bugger
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Persist.Persistence(39));
+            Console.WriteLine(Persist.Persistence(999));
+        }
+    }
+}
+
+public class Persist 
+{
+    public static int Persistence(long n)
+    {
+        string str = n.ToString();
+        int val = 1;
+        int count = 0;
+        while (str.Length != 1)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                val *= int.Parse(str[i].ToString());
+            }
+            str = val.ToString();
+            val = 1;
+            count++;
+        }
+
+        return count;
+    }
+}

485b58afab37d62e919bf16e663c21a99d789ba7
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-11 00:35:57 +0300

    Add files via upload

---
+++ C#/Categorize New Member/Categorize New Member.Test/Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Categorize_New_Member.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            int[][] data = { new int[] { 45, 12 }, new int[] { 55, 21 }, new int[] { 19, 2 }, new int[] { 104, 20 } };
+            List<string> exResult = new List<string>() { "Open", "Senior", "Open", "Senior" };
+
+            List<string> result = Kata.OpenOrSenior(data).ToList();
+            Assert.AreEqual(exResult,result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int[][] data = { new[] { 3, 12 }, new[] { 55, 1 }, new[] { 91, -2 }, new[] { 54, 23 } };
+            List<string> exResult = new List<string>() { "Open", "Open", "Open", "Open" };
+            List<string> result = Kata.OpenOrSenior(data).ToList();
+            Assert.AreEqual(exResult,result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            int[][] data = { new[] { 59, 12 }, new[] { 45, 21 }, new[] { -12, -2 }, new[] { 12, 12 } };
+            List<string> exResult = new List<string>() { "Senior", "Open", "Open", "Open" };
+            List<string> result = Kata.OpenOrSenior(data).ToList();
+            Assert.AreEqual(exResult,result);
+        }
+        
+        [Test]
+        public void Test4()
+        {
+            int[][] data = {new int[] { 45, 12 }};
+            List<string> exResult = new List<string>() { "Open" };
+            List<string> result = Kata.OpenOrSenior(data).ToList();
+            Assert.AreEqual(exResult,result);
+        }
+    }
+}
---
+++ C#/Categorize New Member/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Categorize_New_Member
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var res = Kata.OpenOrSenior(
+                new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } });
+            foreach (var i in res)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+public class Kata
+{
+    public static IEnumerable<string> OpenOrSenior(int[][] data)
+    {
+        List<string> result = new List<string>();
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (data[i][0] >= 55 && data[i][1] > 7)
+            {
+                result.Add("Senior");
+                continue;
+            }
+            result.Add("Open");
+        }
+
+        return result;
+    }
+}

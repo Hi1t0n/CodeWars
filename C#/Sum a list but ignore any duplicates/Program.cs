3091dc40d4b2aaa3882cbe96ff6aaac0471e60b6
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-12 03:00:33 +0300

    Add files via upload

---
+++ C#/Sum a list but ignore any duplicates/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sum_a_list_but_ignore_any_duplicates
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.SumNoDuplicates(new int[]{3,4,3,6}));
+        }
+    }
+}
+
+public class Kata
+{
+    public static int SumNoDuplicates(int[] arr)
+    {
+        int result = 0;
+        Dictionary<int, int> values = new Dictionary<int, int>();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (values.ContainsKey(arr[i]))
+            {
+                values[arr[i]]++;
+                continue;
+            }
+            values.Add(arr[i],1);
+        }
+
+        foreach (var value in values)
+        {
+            if (value.Value != 1)
+            {
+                continue;
+            }
+
+            result += value.Key;
+        }
+        return result;
+    }
+}
---
+++ C#/Sum a list but ignore any duplicates/Sum a list but ignore and duplicates.Tests/Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sum_a_list_but_ignore_and_duplicates
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            int[] arrData = { 1, 1, 2, 3 };
+            int result = Kata.SumNoDuplicates(arrData);
+            int exResult = 5;
+            Assert.AreEqual(result,exResult);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int[] arrData = { 1, 1, 2, 2, 3 };
+            int result = Kata.SumNoDuplicates(arrData);
+            int exResult = 3;
+            Assert.AreEqual(result,exResult);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            int[] arrData = { 3, 4, 3, 6 };
+            int result = Kata.SumNoDuplicates(arrData);
+            int exResult = 10;
+            Assert.AreEqual(result, exResult);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            int[] arrData = { 10, 10, 1, 4, 10, 3, 9, 6, 10, 10, 9, 9, 10, 5, 6, 10, 4, 1 };
+            int result = Kata.SumNoDuplicates(arrData);
+            int exResult = 8;
+            Assert.AreEqual(result,exResult);
+        }
+    }
+}

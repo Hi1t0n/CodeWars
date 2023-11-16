3a79a1f2f89ded039d55561c2366971c2e4cb911
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-17 00:15:28 +0300

    Add files via upload

---
+++ C#/Count of positives sum of negatives/Count of positives sum of negatives.Test/Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Count_of_positives_sum_of_negatives.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            int[] exResult = new int[] { 10, -65 };
+            int[] data = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
+            Assert.AreEqual(exResult,Kata.CountPositivesSumNegatives(data));
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            int[] exResult = new int[] { 8, -50 };
+            int[] data = new int[] {0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14};
+            Assert.AreEqual(exResult,Kata.CountPositivesSumNegatives(data));
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            int[] exResult = new int[] { };
+            int[] data = new int[0];
+            Assert.AreEqual(exResult,Kata.CountPositivesSumNegatives(data));
+        }
+        
+        [Test]
+        public void Test4()
+        {
+            int[] exResult = new int[] { };
+            int[] data = null;
+            Assert.AreEqual(exResult,Kata.CountPositivesSumNegatives(data));
+        }
+    }
+}
---
+++ C#/Count of positives sum of negatives/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Count_of_positives_sum_of_negatives
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = Kata.CountPositivesSumNegatives(null);
+            foreach (var res in result)
+            {
+                Console.Write(res + " ");
+            }
+            result = Kata.CountPositivesSumNegatives(new int[] {0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14});
+            foreach (var res in result)
+            {
+                Console.Write(res + " ");
+            }
+        }
+    }
+}
+
+public class Kata
+{
+    public static int[] CountPositivesSumNegatives(int[] input)
+    {
+        if (input is null || input.Length is 0)
+        {
+            return new int[] { };
+        }
+
+        return new int[] { input.Count(x => x > 0), input.Where(x => x < 0).Sum() };
+    }
+}

394c34a806138fbba66513719c3573eb4dba466c
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-20 01:05:54 +0300

    Add files via upload

---
+++ C#/Sum The Tree/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Sum_The_Tree
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write(Kata.SumTree(new Node(10, new Node(1), new Node(2))));
+        }
+    }
+}
+
+public class Node
+{  
+    public int Value;  
+    public Node Left;  
+    public Node Right;
+    
+    public Node(int value, Node left = null, Node right = null)
+    {
+        Value = value;
+        Left = left;
+        Right = right;
+    }
+}  
+
+public static class Kata
+{
+    public static int SumTree(Node root)
+    {
+        if(root is null){
+            return 0;
+        }
+        return root.Value + SumTree(root.Left) + SumTree(root.Right);
+    }
+}
---
+++ C#/Sum The Tree/Sum The Tree.Tests/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sum_The_Tree.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var data = new Node(10, new Node(1), new Node(2));
+            var exResult = 13;
+            Assert.AreEqual(exResult,Kata.SumTree(data));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var data = new Node(10, new Node(1, null, new Node(15)),new Node(9,new Node(5,null,new Node(5))));
+            var exResult = 45;
+            Assert.AreEqual(exResult,Kata.SumTree(data));
+        }
+    }
+}

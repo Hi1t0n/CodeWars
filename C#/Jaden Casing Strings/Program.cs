752417c794a36438a2980e1ea139e393f9fd792c
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-20 00:25:22 +0300

    Add files via upload

---
+++ C#/Jaden Casing Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Jaden_Casing_Strings
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+
+
+public static class JadenCase
+{
+    public static string ToJadenCase(this string phrase)
+    {
+        string[] words = phrase.Split(' ');
+        
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+        }
+
+        return string.Join(" ", words);
+    }
+}

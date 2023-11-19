e5324a4c097898edf1c36ab444fb7b35fe8f31b2
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-20 00:24:05 +0300

    Add files via upload

---
+++ C#/Return Negative/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Return_Negative
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.MakeNegative(1));
+        }
+    }
+}
+
+public static class Kata
+{
+    public static int MakeNegative(int number) => number > 0 ? number * -1 : number;
+}
---
+++ C#/Return Negative/Return Negative.Tests/Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Return_Negative.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            int data = 1;
+            int exResult = -1;
+            Assert.AreEqual(exResult,Kata.MakeNegative(data));
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            int data = 0;
+            int exResult = 0;
+            Assert.AreEqual(exResult,Kata.MakeNegative(data));
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            int data = -2141;
+            int exResult = -2141;
+            Assert.AreEqual(exResult,Kata.MakeNegative(data));
+        }
+        
+        [Test]
+        public void Test4()
+        {
+            int data = 835980354;
+            int exResult = -835980354;
+            Assert.AreEqual(exResult,Kata.MakeNegative(data));
+        }
+    }
+}

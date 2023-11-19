7c208e8696728a186445acdc25b6393c985e80dd
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-19 18:24:03 +0300

    Add files via upload

---
+++ C#/Unique in Order/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+
+namespace Unique_In_Order
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var results = Kata.UniqueInOrder("AAAABBBCCDAABBB");
+            foreach (var result in results)
+            {
+                Console.Write(result);
+            }
+        }
+    }
+}
+
+public static class Kata
+{
+    public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+    {
+
+        var list = iterable?.ToList();
+        if (list.Count == 0 || list is null)
+        {
+            return list;
+        }
+
+        if (list.Count == 1)
+        {
+            return list;
+        }
+
+        List<T> resultList = new List<T>();
+        var currentElement = list[0];
+        for (int i = 1; i < list.Count; i++)
+        {
+            if (currentElement.Equals(list[i]))
+            {
+                continue;
+            }
+            else
+            {
+                resultList.Add(currentElement);
+                currentElement = list[i];
+            }
+        }
+        
+        resultList.Add(currentElement);
+        
+        if (typeof(T) == typeof(char))
+        {
+            return new string(resultList.Cast<char>().ToArray()) as IEnumerable<T>; 
+        }
+
+        return resultList;
+    }
+}
---
+++ C#/Unique in Order/Unique in Order.Tests/Tests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Unique_in_Order.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var data = "AABBAADD";
+            var exResult = "ABAD";
+            Assert.AreEqual(exResult,Kata.UniqueInOrder(data));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var data = "";
+            var exResult = "";
+            Assert.AreEqual(exResult,Kata.UniqueInOrder(data));
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            var data = "AaaaDDdBbC";
+            var exResult = "AaDdBbC";
+            Assert.AreEqual(exResult, Kata.UniqueInOrder(data));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var data = "1122113344121BbAaaa";
+            var exResult = "12134121BbAa";
+            Assert.AreEqual(exResult, Kata.UniqueInOrder(data));
+        }
+
+        [Test]
+        public void Test5()
+        {
+            int[] data = new int[] { 1, 1, 2, 2, 3, 3, 4 };
+            var exResult = new int[] { 1, 2, 3, 4 };
+            Assert.AreEqual(exResult,Kata.UniqueInOrder(data));
+        }
+    }
+}
+
+

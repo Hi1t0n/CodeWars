013574e52d657bf835edc2405749cb3331d80069
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-20 19:55:22 +0300

    Add files via upload

---
+++ C#/Detect Pangram/Detected Pangram.Tests/Tests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Detected_Pangram.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            string data = "AbCdEfGhIjKlM zYxWvUtSrQpOn";
+            Assert.AreEqual(true,Kata.IsPangram(data));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            Assert.AreEqual(false,Kata.IsPangram(data));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            string data = "";
+            Assert.AreEqual(false,Kata.IsPangram(data));
+        }
+    }
+}

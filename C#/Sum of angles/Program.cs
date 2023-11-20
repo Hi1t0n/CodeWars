5784d169b247d231890eeedd36645f1feacbb614
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-20 18:33:32 +0300

    Add files via upload

---
+++ C#/Sum of angles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+namespace Sum_of_angles
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = Kata.Angle(5);
+            Console.Write(a);
+        }
+    }
+}
+
+public class Kata
+{
+    public static int Angle(int n) => 180 * (n - 2);
+}
---
+++ C#/Sum of angles/Sum of angles.Tests/Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sum_of_angles.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Sum_of_angles_3()
+        {
+            int data = 3;
+            int exResult = 180;
+            Assert.AreEqual(exResult,Kata.Angle(data));
+        }
+
+        [Test]
+        public void Sum_of_angles_4()
+        {
+            int data = 4;
+            int exresult = 360;
+            Assert.AreEqual(exresult,Kata.Angle(data));
+        }
+        
+        [Test]
+        public void Sum_of_angles_5()
+        {
+            int data = 5;
+            int exresult = 540;
+            Assert.AreEqual(exresult,Kata.Angle(data));
+        }
+    }
+}

72e5884efd30c5e57dc47a3384add43d9aa903dd
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-22 15:16:10 +0300

    Add files via upload

---
+++ C#/Sum Strings as Numbers/Sum Strings as Numbers.Tests/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sum_Strings_as_Numbers.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test_127_256_return_383()
+        {
+            string a = "127";
+            string b = "256";
+            string exResult = "383";
+            Assert.AreEqual(exResult,Kata.sumStrings(a,b));
+        }
+        
+        [Test]
+        public void Test_456654456_3213214321412_return_3213670975868()
+        {
+            string a = "456654456";
+            string b = "3213214321412";
+            string exResult = "3213670975868";
+            Assert.AreEqual(exResult,Kata.sumStrings(a,b));
+        }
+
+        [Test]
+        public void Test_Empty_5_return_5()
+        {
+            string a = "";
+            string b = "5";
+            string exResult = "5";
+            Assert.AreEqual(exResult,Kata.sumStrings(a,b));
+        }
+    }
+}
---
+++ C#/Sum Strings as Numbers/Sum Strings as Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Numerics;
+namespace Sum_Strings_as_Numbers
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+
+public static class Kata
+{
+    public static string sumStrings(string a, string b)
+    {
+        BigInteger BigInt1;
+        BigInteger BigInt2;
+
+        BigInteger.TryParse(a, out BigInt1);
+        BigInteger.TryParse(b, out BigInt2);
+
+        return (BigInt1 + BigInt2).ToString();
+
+    }
+}

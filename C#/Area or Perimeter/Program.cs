b75d959cbcbfb51fdcad4ee60c5a0b77bce50d97
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-11-22 14:35:45 +0300

    Add files via upload

---
+++ C#/Area or Perimeter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Area_or_Perimeter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+
+public class MathCheck
+{
+    public static int AreaOrPerimeter(int l, int w) => l == w ? l * w : (l * 2) + (w * 2);
+}
